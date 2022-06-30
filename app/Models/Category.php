33ccf397d3b106bb28ae21a517dc84c83036f9fc
hetRich <95585077+heyIamYi@users.noreply.github.com>
2022-06-30 10:08:52 +0800

    Initial commit

---
+++ app/Models/Category.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $name
+ * @property string $parent_id
+ */
+class Category extends Model
+{
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['created_at', 'updated_at', 'name', 'parent_id'];
+
+    public function childs() {
+        return $this->hasMany('parent_id','id') ;
+}
+
+}
